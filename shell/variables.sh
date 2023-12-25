50aec785736ec9309423f8abedf3bc6673e1c7e6
Saivinay <saivinay165@pec.edu>
2023-12-25 22:38:12 +0530

    eks cluster yaml config file

--- shell/variables.sh
+++ shell/variables.sh
@@ -17,22 +17,17 @@ echo " my name is ${FIRST} "
 # condations in shellscript if else condition
 # check the it sis root user or not
 USERID=$(id)
-USER_ID=$($?)
-if [ $USER_ID -ne 0 ]; then
-      echo "failure"
-   else 
-      echo "sucess"
-   fi
+
 
 
 echo "${USERID}"
 
-# if [ $1 -ne 0 ]; then
-# 		echo -e "$2 ... $R FAILED $N"
-# 		exit 1
-# 	else
-# 		echo -e "$2 ... $G SUCCESS $N"
-# 	fi
+if [ $? -ne 0 ]; then
+		echo "FAILED "
+		exit 1
+	else
+		echo "SUCCESS"
+	fi
 
 
 
