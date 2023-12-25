a2d266cf2d350b81e47abb2aa1faa58a4ed4e388
Saivinay <saivinay165@pec.edu>
2023-12-25 22:03:53 +0530

    eks cluster yaml config file

--- shell/variables.sh
+++ shell/variables.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 
 echo " hi 1st shellscript"
 
@@ -7,3 +7,7 @@ echo $?
 echo "my name is passsing from command line $1 "
 
 echo "my name is passsing from command line $3 "
+
+NAME=${vinay}
+
+echo " my name is ${NAME} "
