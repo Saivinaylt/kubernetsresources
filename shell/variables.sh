0605371627a4bbecd8edc41aa0e664d6c14ad425
Saivinay <saivinay165@pec.edu>
2023-12-25 22:36:51 +0530

    eks cluster yaml config file

--- shell/variables.sh
+++ shell/variables.sh
@@ -19,12 +19,20 @@ echo " my name is ${FIRST} "
 USERID=$(id)
 USER_ID=$($?)
 if [ $USER_ID -ne 0 ]; then
-    echo "failure"
-else 
-    echo "sucess"
-fi
+      echo "failure"
+   else 
+      echo "sucess"
+   fi
+
 
 echo "${USERID}"
+
+# if [ $1 -ne 0 ]; then
+# 		echo -e "$2 ... $R FAILED $N"
+# 		exit 1
+# 	else
+# 		echo -e "$2 ... $G SUCCESS $N"
+# 	fi
 
 
 
