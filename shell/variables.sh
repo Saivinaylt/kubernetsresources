ffdcbc30003398d67a3272c734ea84af457c7af2
Saivinay <saivinay165@pec.edu>
2023-12-26 11:37:01 +0530

    eks cluster yaml config file

--- shell/variables.sh
+++ shell/variables.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-echo " hi 1st shellscript"
+# echo " hi 1st shellscript"
 
-echo $?
+# echo $?
 
-echo "my name is passsing from command line $1 "
+# echo "my name is passsing from command line $1 "
 
-echo "my name is passsing from command line $3 "
+# echo "my name is passsing from command line $3 "
 
-NAME="vinay"
-FIRST="kaki"
+# NAME="vinay"
+# FIRST="kaki"
 
-echo " my name is ${NAME} "
-echo " my name is ${FIRST} "
+# echo " my name is ${NAME} "
+# echo " my name is ${FIRST} "
 
-# condations in shellscript if else condition
-# check the it sis root user or not
-USERID=$(id -u)
+# # condations in shellscript if else condition
+# # check the it sis root user or not
+# USERID=$(id -u)
 
 
 
-echo "${USERID}"
+# echo "${USERID}"
 
-if [ $USERID -ne 0 ]; then
-    echo " ur not a root user"
-    else
-    echo "Sucess=====>"
-    fi
+# if [ $USERID -ne 0 ]; then
+#     echo " ur not a root user"
+#     else
+#     echo "Sucess=====>"
+#     fi
 
 # ouptputredirectries > copy terminal content output to file >> appending &>> both sucess and error appending in file
 # colours coadnif b=30,r=31,g=32,y=33,n=0 nocolur
@@ -41,7 +41,7 @@ echo -e "coloring red ${R} from hear my name ${N} normal"
 
 # functions in shell scripting
 VALIDATE(){
-    echo " hello"
+    echo " hello $1"
 }
 
 VALIDATE
